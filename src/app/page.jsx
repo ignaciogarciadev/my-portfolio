4c345bd82fcd722caaae97d8564ee20fcbe8354b
ignacio-coder <ignacio.coder@gmail.com>
2024-01-04 22:35:54 -0300

    Links and Favicon change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Hero from '../components/Hero'
-import About from '../components/About'
-import Contact from '../components/Contact'
+import Hero from '../pages/Hero'
+import About from '../pages/About'
+import Contact from '../pages/Contact'
 
 
 export default function Home() {
