8cf88f365f9a7938a856fd0d3c4176b1edce6c1d
ignaciogarciadev <ignacio.coder@gmail.com>
2024-01-04 22:43:55 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Hero from '../pages/Hero'
-import About from '../pages/About'
-import Contact from '../pages/Contact'
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Contact from '../components/Contact';
 
 
 export default function Home() {
