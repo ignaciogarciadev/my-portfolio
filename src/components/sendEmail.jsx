76929fb011cd18bb3add24ec85ec987fa27ab522
ignacio-coder <ignacio.coder@gmail.com>
2024-01-04 21:26:34 -0300

    Update sendEmail.jsx

--- src/components/sendEmail.jsx
+++ src/components/sendEmail.jsx
@@ -1,9 +1,9 @@
 // sendEmail.js
 import emailjs from '@emailjs/browser';
 
-const serviceId = process.env.EMAILJS_SERVICE_ID;
-const templateId = process.env.EMAILJS_TEMPLATE_ID;
-const userId = process.env.EMAILJS_USER_ID;
+const serviceId = process.env.NEXT_PUBLIC_SERVICE_ID;
+const templateId = process.env.NEXT_PUBLIC_TEMPLATE_ID;
+const userId = process.env.NEXT_PUBLIC_USER_ID;
 
 const sendEmail = (templateParams) => {
   emailjs.send(serviceId, templateId, templateParams, userId)
